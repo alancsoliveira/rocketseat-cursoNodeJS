6b306f998deccbb9d9cc0b4e5646af1805115e83
Alan Cristian <alancristian964@gmail.com>
2020-02-11 23:32:08 -0300

    Final da Aula 'Conectando Database'

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o app
 const app = express();
 
+// Iniciando o BD
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser: true});
+
+// Primeira rota
 app.get('/', (req,res)=>{
     res.send('Hello NewWorldGames')
 });
