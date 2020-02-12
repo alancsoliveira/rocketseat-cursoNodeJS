d3cbfa3e846e91b90fdb441e9cfc8980e18e4213
Alan Cristian <alancristian964@gmail.com>
2020-02-11 21:44:27 -0300

    Final da Aula 'Instalando MongoDB'

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.send('Hello NewWorldGamesRUN')
+    res.send('Hello NewWorldGames')
 });
 
 app.listen(3001);
