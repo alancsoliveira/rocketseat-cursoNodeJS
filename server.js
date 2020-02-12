fa2be08eb407d4b919eed44626e689040f21726d
Alan Cristian <alancristian964@gmail.com>
2020-02-12 00:34:29 -0300

    Final da Aula 'Criando model de produto'

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Iniciando o app
 const app = express();
 
 // Iniciando o BD
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    });
+// registra o Model na aplicação
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
 
 // Primeira rota
-app.get('/', (req,res)=>{
-    res.send('Hello NewWorldGames')
+app.get('/', (req, res) => {
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+
+    return res.send('Hello NewWorldGames')
 });
 
 app.listen(3001);
---
+++ src/models/Product.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+// Propriedades que serão salvas na base de dados
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+mongoose.model('Product', ProductSchema);
